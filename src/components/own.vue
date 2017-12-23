24d87e3e898e112812376caa1bd5e27ef2eb5eba
wz <562171119@qq.com>
2017-12-23 21:05:37 +0800

    王准提交

--- src/components/own.vue
+++ src/components/own.vue
@@ -1,5 +1,105 @@
 <template>
-  <h3>个人中心</h3>
+  <div>
+    <div><img class="perbg" src="static/img/beijing.jpeg" alt=""></div>
+    <div class="headerPhotoWrap">
+      <img class="headPhoto" :src="src" alt="">
+      <br>
+      <span>嘉木</span>
+    </div>
+    <div >
+      <div class="studyTime"><strong>0小时</strong><br><small>学习时间</small></div>
+      <div class="studyTime"><strong>0小时</strong><br><small>学习时间</small></div>
+      <div class="studyTime"><strong>0小时</strong><br><small>学习时间</small></div>
+      <span>></span>
+    </div>
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item menu-item">
+          <span><img src="static/img/勋章.png" alt=""></span>
+          <div>我的勋章</div>
+          <strong> ></strong>
+        </li>
+        <li class="list-group-item menu-item">
+          <span><img src="static/img/勋章.png" alt=""></span>
+          <div>我的积分</div>
+          <strong> ></strong>
+        </li>
+        <li class="list-group-item menu-item">
+          <span><img src="static/img/勋章.png" alt=""></span>
+          <div>消息中心</div>
+          <strong> ></strong>
+        </li>
+        <li class="list-group-item menu-item">
+          <span><img src="static/img/勋章.png" alt=""></span>
+          <div>意见反馈</div>
+          <strong> ></strong>
+        </li>
+        <li class="list-group-item menu-item">
+          <span><img src="static/img/勋章.png" alt=""></span>
+          <div>使用规则</div>
+          <strong> ></strong>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
 </template>
-<script></script>
-<style></style>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        src:  "static/img/touxiang.jpg"
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .perbg{
+    width: 3.75rem;
+    height: 1.5rem;
+
+  }
+  .headPhoto{
+    width: .8rem;
+    height: .8rem;
+    border-radius: 50%;
+    margin-top: -.4rem;
+  }
+  .headerPhotoWrap{
+    text-align: center;
+    /*width: .8rem;*/
+    /*height: 1.2rem;*/
+  }
+  .studyTime{
+    display: inline-block;
+    width: 30%;
+
+  }
+  .menu-item>span>img{
+    display: inline-block;
+      width: .2rem;
+    height: .2rem;
+
+  }
+  .menu-item>div{
+    text-indent: .2rem;
+    display: inline-block;
+    width: 88%;
+    margin: 0;
+    top: 0;
+    text-align: left;
+  }
+
+  .menu-item>strong{
+    width: 20%;
+   }
+
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -18,7 +18,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-
   router,
   template: '<App/>',
   components: { App }
