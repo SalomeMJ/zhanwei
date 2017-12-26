bfc3f0b891644d6486c5f5711647b9e2d05d86f2
Administrator <2825027346@qq.com>
2017-12-26 11:08:14 +0800

    马静文-导航

--- src/components/home.vue
+++ src/components/home.vue
@@ -13,11 +13,11 @@
             <span class="rightLine"></span>
           </div>
           <ul>
-            <li>留学总体规划</li>
+            <li class="goout">留学总体规划</li>
             <li>院校学术要求</li>
             <li>留学准备建议</li>
           </ul>
-          <span>点击免费领取</span>
+          <span class="clickFree">点击免费领取</span>
         </div>
         <a href="https://www.baidu.com/">
           <el-carousel >
@@ -26,7 +26,65 @@
             </el-carousel-item>
           </el-carousel>
         </a>
-
+      <ul class="contentItem">
+        <li>
+          <div>
+            <img src="../assets/c1.png" alt="">
+            <span>监督占位</span>
+          </div>
+          <div>
+            <img src="../assets/c2.png" alt="">
+            <span>使用记录</span>
+          </div>
+          <div>
+            <img src="../assets/c3.png" alt="">
+            <span>积分商城</span>
+          </div>
+          <div>
+            <img src="../assets/c4.png" alt="">
+            <span>道具包</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../assets/c5.png" alt="">
+            <span>积分任务</span>
+          </div>
+          <div>
+            <img src="../assets/c6.png" alt="">
+            <span>拍行榜</span>
+          </div>
+          <div>
+            <img src="../assets/c7.png" alt="">
+            <span>违规记录</span>
+          </div>
+          <div>
+            <img src="../assets/c8.png" alt="">
+            <span>小礼包</span>
+          </div>
+        </li>
+      </ul>
+      <div class="proceed">
+        <span class="zuowei">座位预定</span>
+        <div class="btns">
+          <button>常用座位1</button>
+          <button>常用座位2</button>
+        </div>
+        <img src="../assets/setting.png" alt="">
+        <ul>
+          <li>
+            <div class="left">
+              <span>2楼206综合图书馆(2楼)</span>
+              <span>8:00~22:00开关前可提前2.0小时预订</span>
+            </div>
+            <div class="right">
+              <span>0</span>
+              <span>/7</span>
+              <p>已满</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
 
   </div>
@@ -50,10 +108,12 @@
     }
   }
 </script>
-<style>
+    <style>
   ul,li{
     list-style: none;
     display: inline-block;
+    margin: 0;
+    padding: 0;
   }
   .el-carousel__item h3 {
     display: inline-block;
@@ -115,6 +175,108 @@
     right:0.05rem;
     z-index: 9999;
   }
+  .content{
+    width: 100%;
+    margin: 0.5rem auto;
+    /*height:1rem;*/
+    /*border:0.01rem solid red;*/
+    color:#fff;
+    position: absolute;
+    top: 0.2rem;
+    z-index: 9999;
+    font-size: 0.18rem;
+  }
+  .content .title{
+    /*border:0.01rem solid red;*/
+  }
+    .content .title .leftLine{
+      display: inline-block;
+      width: 10%;
+      height: 0.01rem;
+      background-color: #fff;
+      border:0.01rem solid #fff;
+      vertical-align:middle;
+    }
+  .content .title .rightLine{
+    display: inline-block;
+    width: 10%;
+    height: 0.01rem;
+    background-color: #fff;
+    border:0.01rem solid #fff;
+    vertical-align:middle;
+  }
+.content ul{
+/*border:0.01rem solid red;*/
+  width: 100%;
+  margin-left: -0.2rem;
+}
+  .content ul li{
+    display: inline-block;
+    width: 30%;
+    font-size: 0.12rem;
+    /*border:0.01rem solid blue;*/
+    margin-left: -0.06rem;
+    border-left:0.01rem solid #fff;
+
+  }
+      .content ul  .goout{
+        border:none;
+      }
+.clickFree{
+  display: block;
+  font-size: 0.15rem;
+}
+  ul.el-carousel__indicators {
+    display: none;
+  }
+  ul.contentItem {
+    width: 100%;
+    height: 1.5rem;
+    /*border: 0.01rem solid red;*/
+    border-bottom:0.01rem solid #999;
+  }
+      ul.contentItem li{
+        width: 100%;
+        height: 0.7rem;
+        /*border:0.01rem solid blue;*/
+        /*text-align: left;*/
+      }
+      ul.contentItem li div{
+        display: inline-block;
+        width: 24%;
+        height: 0.7rem;
+        /*border:0.01rem solid green;*/
+        font-size: 0.12rem;
+        /*text-align: center;*/
+      }
+      ul.contentItem li div img{
+        display: block;
+        width: 0.4rem;
+        height: 0.4rem;
+        margin: 0 auto;
+        margin-top: 0.1rem;
+        /*border:0.01rem solid blue;*/
+      }
+.proceed{
+  width: 100%;
+  background:#f8f8f8;
+}
+.proceed .zuowei{
+  display: block;
+  width: 30%;
+  height: 0.3rem;
+  line-height: 0.3rem;
+  border-top:0.01rem solid red;
+  border-left:0.01rem solid red;
+  border-top-left-radius: 0.3rem;
+  border-top-right-radius: 0.3rem;
+ /*border:0.01rem solid red;*/
+  /*background-color: #fff;*/
+
+}
+
+
+
 </style>
 
 
